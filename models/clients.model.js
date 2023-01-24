0727ac13a1e21d457cd124138dcf0b386a7a74e4
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-01-24 02:04:47 +0100

    rutas, modelos y request para obtener por id clientes y para añadir clientes

--- models/clients.model.js
+++ models/clients.model.js
@@ -2,6 +2,16 @@ const getAll = () => {
     return db.query('SELECT * FROM clients');
 }
 
+const getById = (clientId) => {
+    return db.query('SELECT * FROM clients WHERE idClient = ?', [clientId])
+}
+
+const createClient = ({ name, surname, address, phone, birthdate, email, dni }) => {
+    return db.query(
+        'INSERT INTO clients (name, surname, address, phone, birthdate, email, dni) VALUES (?, ?, ?, ?, ?, ?, ?)', [name, surname, address, phone, birthdate, email, dni]
+    );
+}
+
 module.exports = {
-    getAll
+    getAll, getById, createClient
 }
--- routes/api/clients.js
+++ routes/api/clients.js
@@ -1,4 +1,4 @@
-const { getAll } = require('../../models/clients.model');
+const { getAll, getById, createClient } = require('../../models/clients.model');
 
 const router = require('express').Router();
 
@@ -9,6 +9,32 @@ router.get('/', async (req, res) => {
     } catch (error) {
         res.json({ fatal: error.message });
     }
-})
+});
+
+router.get('/:clientId', async (req, res) => {
+    const { clientId } = req.params;
+
+    try {
+        const [client] = await getById(clientId);
+        if (client.length === 0) {
+            res.json({
+                fatal: 'No existe el cliente'
+            });
+        }
+        res.json(client[0]);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const [result] = await createClient(req.body);
+        const [client] = await getById(result.insertId);
+        res.json(client[0]);
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+});
 
 module.exports = router;
