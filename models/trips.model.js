fe4d7c3e8bc5c238b258ba43f6508dfb006cf7a0
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-01-25 16:05:12 +0100

    todas las rutas y métodos de trips

--- models/trips.model.js
+++ models/trips.model.js
@@ -1,5 +1,22 @@
 const getAllTrips = () => {
-    return db.query('SELECT * FROM trips');
+    return db.query('SELECT * FROM trips')
 }
 
-module.exports = { getAllTrips }
+const getTripById = (tripId) => {
+    return db.query('SELECT * FROM trips WHERE idtrip = ?', [tripId])
+}
+
+const createTrip = ({ departureDate, arrivalDate, idDepartureFlight, idArrivalFlight, idHotel }) => {
+    return db.query(
+        'INSERT INTO trips (departureDate, arrivalDate, idDepartureFlight, idArrivalFlight, idHotel) VALUES (?, ?, ?, ?, ?)', [departureDate, arrivalDate, idDepartureFlight, idArrivalFlight, idHotel])
+}
+
+const updateTrip = (tripId, { departureDate, arrivalDate, idDepartureFlight, idArrivalFlight, idHotel }) => {
+    return db.query('UPDATE trips SET departureDate = ?, arrivalDate = ?, idDepartureFlight = ?, idArrivalFlight = ?, idHotel = ? WHERE idtrip = ?', [departureDate, arrivalDate, idDepartureFlight, idArrivalFlight, idHotel, tripId])
+}
+
+const deleteTrip = (tripId) => {
+    return db.query('DELETE FROM trips WHERE idtrip = ?', [tripId])
+}
+
+module.exports = { getAllTrips, getTripById, createTrip, updateTrip, deleteTrip }
--- routes/api/trips.js
+++ routes/api/trips.js
@@ -1,5 +1,5 @@
 const { getHotelById } = require('../../models/hotels.model');
-const { getAllTrips } = require('../../models/trips.model');
+const { getAllTrips, getTripById, createTrip, updateTrip, deleteTrip } = require('../../models/trips.model');
 
 const router = require('express').Router();
 
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
         for (let trip of trips) {
             const [hotel] = await getHotelById(trip.idHotel);
             trip.hotel = hotel[0];
-            delete trip.idHotel
+            delete trip.idHotel;
         }
         res.json(trips);
     } catch (error) {
@@ -17,5 +17,48 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:tripId', async (req, res) => {
+    const { tripId } = req.params;
+    try {
+        const [trip] = await getTripById(tripId);
+        const [hotel] = await getHotelById(trip[0].idHotel);
+        trip[0].hotel = hotel[0];
+        delete trip[0].idHotel;
+        res.json(trip);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const [result] = await createTrip(req.body);
+        const [trip] = await getTripById(result.insertId);
+        res.json(trip[0]);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.put('/:tripId', async (req, res) => {
+    const { tripId } = req.params;
+    try {
+        const [result] = await updateTrip(tripId, req.body);
+        const [trip] = await getTripById(tripId);
+        res.json(trip[0]);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.delete('/:tripId', async (req, res) => {
+    const { tripId } = req.params;
+    try {
+        const [result] = await deleteTrip(tripId);
+        res.json({ message: 'Viaje borrado' });
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
 
 module.exports = router;
